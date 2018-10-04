1a2f9138926849d5ba500dca066c6a84585d87c7
Will Figueiredo <will@wfigueiredo.com>
2018-10-04 03:22:51 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -107,8 +107,6 @@ def changing_alan
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 		 programmer_hash
-
-
 end
 
 def adding_to_dennis
@@ -128,4 +126,5 @@ def adding_to_dennis
         }
      }
 	programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+	programmer_hash
 end
